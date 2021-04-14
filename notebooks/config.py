246869c091c0608c00c1485f68c825c65c1730cb
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-04-14 16:07:32 -0400

    add rix's notebook, with my edits

---
+++ notebooks/config.py
@@ -0,0 +1,34 @@
+font = {
+    'family': 'monospace',
+    'color':  'black',
+    'weight': 'bold',
+    'size': 20,
+}
+
+cmap_r = 'inferno_r'
+cmap = 'inferno'
+
+style = {
+    'font.family': 'serif',
+    'font.size': 22,
+    "axes.titlesize": "medium",
+    "axes.labelsize": "medium",
+    'axes.edgecolor': '#000000',
+    "xtick.direction": "out",
+    "ytick.direction": "out",
+    #"xtick.major.size": "8",
+    #"xtick.minor.size": "4",
+    #"ytick.major.size": "8",
+    #"ytick.minor.size": "4",
+    'xtick.labelsize': 'medium',
+    'ytick.labelsize': 'medium',
+    'ytick.color': '#000000',
+    'xtick.color': '#000000',
+    'xtick.top': False,
+    'ytick.right': False,
+    'axes.spines.top': True,
+    'axes.spines.right': True,
+    'axes.linewidth': 1.5,
+    'image.aspect': 'auto',
+    'figure.dpi':80
+}
